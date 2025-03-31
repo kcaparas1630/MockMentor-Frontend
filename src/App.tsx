07f6d7f2d2e20c2839597416f731925a608ba51d
Kent Caparas <kcaparas1630@gmail.com>
2025-03-30 18:25:18 -0700

    successful control of dynamic component with isDarkMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Dispatch, SetStateAction, useState } from "react";
+
+const DarkModeToggle = ({ isDarkMode, setIsDarkMode }: { 
+  isDarkMode: boolean, 
+  setIsDarkMode: Dispatch<SetStateAction<boolean>> 
+}) => {
+  const handleDarkModeToggle = () => {
+    setIsDarkMode(!isDarkMode);
+  }
+  return <button className="bg-white dark:bg-black text-black dark:text-white" onClick={handleDarkModeToggle}>Toggle Dark Mode</button>
+}
+
 const App = () => {
-  return <div className="bg-white dark:bg-black text-black dark:text-white">Hello World</div>
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  return (
+    <div className={`${isDarkMode ? 'text-white bg-black' : 'text-black bg-white'}`}>
+      <h1 className={`${isDarkMode ? 'text-white' : 'text-black'}`}> Hello World </h1>
+      <DarkModeToggle isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+    </div>
+  )
 }
 
 export default App
