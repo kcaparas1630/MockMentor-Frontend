99c06dbbc1772a9ddd3a7bb374e084dbe7be01d9
Kent Caparas <kcaparas1630@gmail.com>
2025-05-09 10:20:32 -0700

    interface refactored

--- src/Types/Forms/Input.ts
+++ src/Types/Forms/Input.ts
@@ -3,6 +3,8 @@ interface InputProps {
   type: string;
   placeholder: string;
   label: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default InputProps;
