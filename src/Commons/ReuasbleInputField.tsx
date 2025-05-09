2377da5be32bc57f8ae18982f663dce476766153
Kent Caparas <kcaparas1630@gmail.com>
2025-05-09 10:18:57 -0700

    refactored reusable input for firebase use

--- src/Commons/ReuasbleInputField.tsx
+++ src/Commons/ReuasbleInputField.tsx
@@ -1,14 +1,13 @@
 import { FC } from "react";
 import InputProps from "../Types/Forms/Input";
 import { Label, InputField, InputContainer } from "./Styles/StyledInput";
-import { useFormContext } from "react-hook-form";
 
-const ReusableInput: FC<InputProps> = ({ name, type, placeholder, label }) => {
-  const { register } = useFormContext();
+const ReusableInput: FC<InputProps> = ({ name, type, placeholder, label, value, onChange }) => {
+
   return (
     <InputContainer>
       <Label htmlFor={name}>{label}</Label>
-      <InputField type={type} placeholder={placeholder} {...register(name)} />
+      <InputField type={type} placeholder={placeholder} value={value} onChange={onChange} />
     </InputContainer>
   );
 };
