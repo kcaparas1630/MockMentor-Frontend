1c5dab7d97b477a436f339fa6efbaef18a5a503a
kcaparas1630 <kcaparas1630@gmail.com>
2025-05-29 10:56:09 -0700

    added spinner style

---
+++ src/Commons/Styles/StyledSpinner.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+const Spinner = styled.div`
+  width: 24px;
+  height: 24px;
+  border: 3px solid #2C2F33;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export default Spinner;
