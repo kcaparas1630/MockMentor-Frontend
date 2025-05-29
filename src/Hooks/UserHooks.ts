632409615f76d4825c614e943beff3e49f2736bc
kcaparas1630 <kcaparas1630@gmail.com>
2025-05-29 10:56:26 -0700

    added custom hooks for getting user and updating user

---
+++ src/Hooks/UserHooks.ts
@@ -0,0 +1,63 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { auth } from "../Firebase/FirebaseAuth";
+import axios from "axios";
+import ProfileData from "@/Types/ProfileData";
+
+
+const getUserToken = async (): Promise<string> => {
+    const user = auth.currentUser;
+    if (user) {
+        return user.getIdToken();
+    }
+    return "";
+}
+const getUser = async (): Promise<ProfileData> => {
+    try {
+        const userToken = await getUserToken();
+        const response = await axios.get('http://localhost:3000/api/user', {
+            headers: {
+                'Authorization': `Bearer ${userToken}`
+            }
+        })
+        return response.data;
+    } catch (error) {
+        console.error(error); // TODO: Handle error
+        throw error;
+    }
+}
+
+const UpdateUser = async (userData: ProfileData) => {
+    try {
+        const userToken = await getUserToken();
+        const response = await axios.put('http://localhost:3000/api/update-user',
+            userData,
+            {
+            headers: {
+                'Authorization': `Bearer ${userToken}`
+            }
+        })
+        return response.data;
+    } catch (error) {
+        console.error(error); // TODO: Handle error
+        throw error;
+    }
+}
+
+const GetUserQuery = () => {
+    const {
+        data: users,
+        isPending,
+        isError,
+    }: UseQueryResult<ProfileData, Error> = useQuery({
+        queryKey: ['user'],
+        queryFn: getUser,
+        enabled: !!auth.currentUser,
+    })
+    return {
+        users,
+        isPending,
+        isError,
+    }
+}
+
+export { GetUserQuery, UpdateUser };
