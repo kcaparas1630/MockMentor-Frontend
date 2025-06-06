99f61c09e32c369e35489f2880883579e32e186f
kcaparas1630 <kcaparas1630@gmail.com>
2025-06-06 11:53:26 -0700

    created a component to guard routes

---
+++ src/Components/AuthGuard.tsx
@@ -0,0 +1,37 @@
+
+import { useEffect, useState } from 'react';
+import { useNavigate } from '@tanstack/react-router';
+import { auth } from '../Firebase/FirebaseAuth';
+import LoadingSpinner from '../Commons/Spinner';
+
+interface AuthGuardProps {
+    children: React.ReactNode;
+}
+
+const AuthGuard = ({ children }: AuthGuardProps) => {
+    const [loading, setLoading] = useState(true);
+    const [authenticated, setAuthenticated] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            console.log("auth.currentUser", user);
+            if (user) {
+                setAuthenticated(true);
+            } else {
+                navigate({ to: '/login', replace: true });
+            }
+            setLoading(false);
+        });
+
+        return unsubscribe;
+    }, [navigate]);
+
+    if (loading) {
+        return <LoadingSpinner />;
+    }
+
+    return authenticated ? <>{children}</> : null;
+};
+
+export default AuthGuard;
