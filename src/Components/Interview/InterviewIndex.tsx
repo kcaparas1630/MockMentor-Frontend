5a6d2f9b1ef91c355707f73fcb03320b494dd4c3
kcaparas1630 <kcaparas1630@gmail.com>
2025-06-07 12:04:57 -0700

    added todo comments for interview index

--- src/Components/Interview/InterviewIndex.tsx
+++ src/Components/Interview/InterviewIndex.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import VideoTestCard from "./VideoTestCard";
+
 const InterviewIndex: FC = () => {
+    // TODO: add conditional statement to check if session is active. if not, redirect to video test card.
     return (
         <VideoTestCard />
     )
