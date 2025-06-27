f41dd8223efe729b86a869f67fccd10d6d1a5883
Kent Caparas <kcaparas1630@gmail.com>
2025-06-26 21:24:52 -0700

    added paths for resolving alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -38,5 +39,10 @@ export default defineConfig({
     watch: {
       usePolling: true
     }
-  }
+  },
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+  },
 })
