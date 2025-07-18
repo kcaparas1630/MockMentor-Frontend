072ffb67608a40c71c9879be68183106567d829c
kcaparas1630 <kcaparas1630@gmail.com>
2025-07-18 11:55:46 -0700

    wrapped the application inside the calibration provider

--- src/main.tsx
+++ src/main.tsx
@@ -28,6 +28,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 // import the generated  route tree
 import { routeTree } from './routeTree.gen'
 import { MediaDevicesProvider } from './Context/MediaDevicesContext'
+import { CalibrationProvider } from './Context/CalibrationContext'
 
 /**
  * Query client instance for managing server state and caching.
@@ -78,7 +79,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <MediaDevicesProvider>
-        <RouterProvider router={router} />
+        <CalibrationProvider>
+          <RouterProvider router={router} />
+        </CalibrationProvider>
       </MediaDevicesProvider>
     </QueryClientProvider>
   </StrictMode>,
