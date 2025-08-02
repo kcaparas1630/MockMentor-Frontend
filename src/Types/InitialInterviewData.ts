c55ccdf2cba0006bb72e6023c8947a7778ea5584
Kent Caparas <kcaparas1630@gmail.com>
2025-08-02 10:27:26 -0700

    Created an interface to make the initial data type-safe

---
+++ src/Types/InitialInterviewData.ts
@@ -0,0 +1,10 @@
+interface InitialInterviewData {
+  session_id: string;
+  user_name: string;
+  jobRole: string | undefined;
+  jobLevel: string;
+  questionType: string;
+  aiInstructions: string;
+}
+
+export default InitialInterviewData;
